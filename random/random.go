e27aa8b114a235daa3bf908b2de2c0fa52228757
John Law <johnlaw.po@gmail.com>
2021-06-04 23:42:43 +0800

    feat: generate random string

--- random/random.go
+++ random/random.go
@@ -2,13 +2,29 @@ package random
 
 import (
 	"fmt"
+	"math/rand"
+	"time"
 
 	"github.com/PaulSonOfLars/gotgbot/v2"
 	"github.com/PaulSonOfLars/gotgbot/v2/ext"
 )
 
+var letters = []rune("_0123456789abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ")
+
+func gen_random_string(n int) string {
+	b := make([]rune, n)
+	for i := range b {
+		b[i] = letters[rand.Intn(len(letters))]
+	}
+	return string(b)
+}
+
 func GetRandom(b *gotgbot.Bot, ctx *ext.Context) error {
-	msg := "random_string"
+	rand.Seed(time.Now().UTC().UnixNano())
+	msg := "0"
+	for msg[0] >= '0' && msg[0] <= '9' {
+		msg = gen_random_string(30)
+	}
 	if _, err := b.SendMessage(ctx.EffectiveChat.Id,
 		msg,
 		&gotgbot.SendMessageOpts{ParseMode: "html"}); err != nil {
