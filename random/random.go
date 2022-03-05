9ea805bf17ac6d98dade96ff1048121ea246eadb
John Law <johnlaw.po@gmail.com>
2022-03-05 19:07:06 +0800

    random/fix: avoid leading or trailing underscores

--- random/random.go
+++ random/random.go
@@ -15,7 +15,7 @@ func gen_random_string(n int) string {
 	b := make([]rune, n)
 	for i := range b {
 		b[i] = letters[rand.Intn(len(letters))]
-		for i > 0 && b[i-1] == '_' && b[i] == '_' {
+		for (i > 0 && b[i-1] == '_' && b[i] == '_') || ((i == n-1 || i == 0) && b[i] == '_') {
 			b[i] = letters[rand.Intn(len(letters))]
 		}
 	}
